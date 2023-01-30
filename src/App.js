e5a628a4b05f808809be3d06baf4d58968760966
ChaimaGharbi <Chaima.GH6602@gmail.com>
2023-01-30 18:57:45 +0100

    fixed

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import logo from './logo.svg';
 import './App.css';
 
@@ -19,6 +20,32 @@ function App() {
         </a>
       </header>
     </div>
+=======
+import "./App.css";
+import React from "react";
+import Products from "./components/Products";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Fab from "./components/Fab";
+import AddProduct from "./components/AddProduct";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Products name="products"></Products>
+              <Fab></Fab>
+            </>
+          }
+        ></Route>
+        <Route path="/addProduct" element={<AddProduct></AddProduct>}></Route>
+        <Route path="/orders" element={<Products name="order"></Products>}></Route>
+      </Routes>
+    </Router>
+>>>>>>> 723febf7707b5f54c6f4b2f5b00596b40459d37b
   );
 }
 
